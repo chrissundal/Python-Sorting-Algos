6c62ced84bcc3d92f77ab1712a0eb2062a4e8903
chrissundal <christoffersj@hotmail.com>
2025-04-26 09:41:20 +0200

    Getting started with pygame ui for the sorting

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ class DrawInformation:
     BACKGROUND_COLOR = WHITE
     SIDE_PAD = 100
     TOP_PAD = 150
+    GRADIENTS = [(128, 128, 128), (160, 160, 160), (192, 192, 192)]
 
     def __init__(self, width, height, lst):
         self.width = width
@@ -26,5 +27,39 @@ class DrawInformation:
         self.min_value = min(lst)
         self.block_width = round((self.width - self.SIDE_PAD) / len(lst))
         self.block_height = round((self.height - self.TOP_PAD) / (self.max_value - self.min_value))
-        
+        self.start_x = self.SIDE_PAD // 2
 
+def draw(draw_info):
+    draw_info.window.fill(draw_info.BACKGROUND_COLOR)
+    pygame.display.update()
+
+def draw_list(draw_info):
+    lst = draw_info.lst
+
+    for i, value in enumerate(lst):
+        x = draw_info.start_x + i * draw_info.block_width
+        y = draw_info.height - (value - draw_info.min_value) * draw_info.block_height
+def generate_starting_list(size):
+    lst = [random.randint(0, size) for _ in range(size)]
+    return lst
+
+def main():
+    run = True
+    clock = pygame.time.Clock()
+
+    draw_info = DrawInformation(800, 600, generate_starting_list(100))
+
+    while run:
+        clock.tick(60)
+        draw(draw_info)
+        pygame.display.update()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
